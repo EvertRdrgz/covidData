1871375d85e54ecb4f5db51ab5270816897957a6
Evert Rodriguez <everodriguez@csumb.edu>
2021-08-07 01:51:14 -0700

    basic i/o out data from local file

--- hw.py
+++ hw.py
@@ -1,1 +1,24 @@
-print("Hello World")
+import csv
+
+data = "Mexico.csv"
+fields = []
+rows = []
+pop = 127600000 #from google as of 2009
+with open(data, 'r') as file:
+    csvreader = csv.reader(file)
+
+    fields = next(csvreader)
+
+    for row in csvreader:
+        rows.append(row)
+data_points = csvreader.line_num-1
+
+print("There are this many data points: %d"%data_points)
+print('The field names are: ' + ', '.join(field for field in fields))
+
+
+fully_vax = rows[data_points-1]
+print("\nThe total number of fully vaxinated people in " + fully_vax[0] +" as of " + fully_vax[1] + " is: " + fully_vax[len(fully_vax)-1])
+vax_pop = int(fully_vax[len(fully_vax)-1])
+pervax = str((vax_pop/pop)*100)
+print("This means that " + pervax + " of the population is protected from COVID-19")
